da3df78606489017c00c538c23ba80bcd40f051f
baharcos <baharcosk@gmail.com>
2023-05-22 21:49:18 +0200

    Add the DataLoader.

--- src/political_party_analysis/loader.py
+++ src/political_party_analysis/loader.py
@@ -3,6 +3,8 @@ from typing import List
 from urllib.request import urlretrieve
 
 import pandas as pd
+from sklearn.impute import KNNImputer
+from sklearn.preprocessing import StandardScaler
 
 
 class DataLoader:
@@ -24,29 +26,47 @@ class DataLoader:
 
     def remove_duplicates(self, df: pd.DataFrame) -> pd.DataFrame:
         """Write a function to remove duplicates in a dataframe"""
-        ##### YOUR CODE GOES HERE #####
-        pass
+
+        df = df.drop_duplicates() 
+        return df
+        
 
     def remove_nonfeature_cols(
         self, df: pd.DataFrame, non_features: List[str], index: List[str]
     ) -> pd.DataFrame:
         """Write a function to remove certain features cols and set certain cols as indices
         in a dataframe"""
-        ##### YOUR CODE GOES HERE #####
-        pass
+        
+        df.drop(non_features, axis=1, inplace=True)
+        df.set_index(index, inplace=True) #eastwest - exception iceland(would make it 1)
+        return df
+       
 
     def handle_NaN_values(self, df: pd.DataFrame) -> pd.DataFrame:
         """Write a function to handle NaN values in a dataframe"""
-        ##### YOUR CODE GOES HERE #####
-        pass
+    
+        threshold = int(len(df) / 2) # may need to be adjusted
+        df.dropna(axis=1, thresh=threshold, inplace=True) #turkey
+        
+        imputer = KNNImputer(n_neighbors=3, weights="distance")
+        df_imputed = pd.DataFrame(imputer.fit_transform(df), columns=df.columns, index=df.index)
+        
+        return df_imputed
     
     def scale_features(self, df: pd.DataFrame) -> pd.DataFrame:
         """Write a function to normalise values in a dataframe. Use StandardScaler."""
-        ##### YOUR CODE GOES HERE #####
-        pass
+
+        scaler = StandardScaler()
+        normalized_values = scaler.fit_transform(df.values)
+        normalized_df = pd.DataFrame(normalized_values, columns=df.columns, index=df.index)
+
+        return normalized_df
 
 
     def preprocess_data(self):
         """Write a function to combine all pre-processing steps for the dataset"""
-        ##### YOUR CODE GOES HERE #####
-        pass
+        
+        self.party_data = self.remove_duplicates(self.party_data)
+        self.party_data = self.remove_nonfeature_cols(self.party_data, self.non_features, self.index)
+        self.party_data = self.handle_NaN_values(self.party_data)
+        self.party_data = self.scale_features(self.party_data)
