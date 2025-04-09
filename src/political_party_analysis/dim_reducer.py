12c98865484bf14dedbd62afa5d788cf8d448708
Padmanabhan Manoharan <stevmath@gmail.com>
2025-04-09 14:01:28 +0530

    Remove unused variable assignment

--- src/political_party_analysis/dim_reducer.py
+++ src/political_party_analysis/dim_reducer.py
@@ -9,7 +9,6 @@ class DimensionalityReducer:
     def __init__(self, data: pd.DataFrame, n_components: int = 2):
         self.n_components = n_components
         self.data = data
-        self.feature_columns = data.columns
 
     ##### YOUR CODE GOES HERE #####
     def transform(self):
