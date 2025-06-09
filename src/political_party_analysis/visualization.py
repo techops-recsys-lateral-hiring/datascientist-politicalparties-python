9794eecd3b3f16ada5dad9e4dbbc0741815cb780
Ned Letcher <nletcher@gmail.com>
2025-06-10 01:22:08 +1000

    improve typing and imports for visualization.py

--- src/political_party_analysis/visualization.py
+++ src/political_party_analysis/visualization.py
@@ -1,22 +1,21 @@
 from itertools import cycle
-from typing import List, Optional
 
-import matplotlib as mpl
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-from matplotlib import pyplot
+from matplotlib.patches import Ellipse
 
 
 def scatter_plot(
     transformed_data: pd.DataFrame,
     color: str = "y",
     size: float = 2.0,
-    splot: pyplot.subplot = None,
-    label: Optional[List[str]] = None,
-):
+    splot: plt.Axes | None = None,
+    label: list[str] | None = None,
+) -> None:
     """Write a function to generate a 2D scatter plot."""
     if splot is None:
-        splot = pyplot.subplot()
+        splot = plt.subplot()
     columns = transformed_data.columns
     splot.scatter(
         transformed_data.loc[:, columns[0]],
@@ -37,11 +36,11 @@ def plot_density_estimation_results(
     means: np.ndarray,
     covariances: np.ndarray,
     title: str,
-):
+) -> None:
     """Use this function to plot the estimated distribution"""
     color_iter = cycle(["navy", "c", "cornflowerblue", "gold", "darkorange", "g"])
-    pyplot.figure()
-    splot = pyplot.subplot()
+    plt.figure()
+    splot = plt.subplot()
     for i, (mean, covar, color) in enumerate(zip(means, covariances, color_iter)):
         v, w = np.linalg.eigh(covar)
         v = 2.0 * np.sqrt(2.0) * np.sqrt(v)
@@ -51,14 +50,16 @@ def plot_density_estimation_results(
         scatter_plot(X.loc[Y_ == i], color=color, splot=splot)
         angle = np.arctan(u[1] / u[0])
         angle = 180.0 * angle / np.pi
-        ell = mpl.patches.Ellipse(mean, v[0], v[1], angle=180.0 + angle, color=color)
+        ell = Ellipse(mean, v[0], v[1], angle=180.0 + angle, color=color)
         ell.set_clip_box(splot.bbox)
         ell.set_alpha(0.5)
         splot.add_artist(ell)
-    pyplot.title(title)
+    plt.title(title)
 
 
-def plot_finnish_parties(transformed_data: pd.DataFrame, splot: pyplot.subplot = None):
+def plot_finnish_parties(
+    transformed_data: pd.DataFrame, splot: plt.Axes | None = None
+) -> None:
     """Write a function to plot the following finnish parties on a 2D scatter plot"""
     finnish_parties = [
         {"parties": ["SDP", "VAS", "VIHR"], "country": "fin", "color": "r"},
@@ -67,4 +68,3 @@ def plot_finnish_parties(transformed_data: pd.DataFrame, splot: pyplot.subplot =
         {"parties": ["PS"], "country": "fin", "color": "k"},
     ]
     ##### YOUR CODE GOES HERE #####
-    pass
